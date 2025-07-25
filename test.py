d5e9f516078ba1d50fda23b4028edf022b286dd4
Truong The Tai <truongtai93@gmail.com>
2025-07-25 09:23:50 +0700

    yufgffh

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
-import pickle
+print(str(" https://sgp1-4.download.real-debrid.com/d/BCZSMWWJEN3MK45/The.Last.of.Us.S02E03.The.Path.2160p.MAX.WEB-DL.DDP5.1.Atmos.DV.HDR.H.265-G66.mkv".rsplit("/", 1)[1].rsplit(".", 1)))
 
+"The.Last.of.Us.S02E03.The.Path.2160p.MAX.WEB-DL.DDP5.1.Atmos.DV.HDR.H.265-G66.vie.srt"
+"The.Last.of.Us.S02E03.The.Path.2160p.MAX.WEB-DL.DDP5.1.Atmos.DV.HDR.H.265-G66.vi.srt"
 # Replace 'your_file.pkl' with the actual path to your PKL file
-file_path = 'C:/Users/Admin/Downloads/Programs/last_played/plugin.video.last_played/repo/a4kSubtitles-master/a4kSubtitles/lib/third_party/iso639/data/iso-639_langs.pkl'
-try:
-    with open(file_path, 'rb') as file:
-        data = pickle.load(file)
-    print(data) # This will print the loaded Python object
-except Exception as e:
-    print(f"Error loading pickle file: {e}")
+# file_path = 'C:/Users/Admin/Downloads/Programs/last_played/plugin.video.last_played/repo/a4kSubtitles-master/a4kSubtitles/lib/third_party/iso639/data/iso-639_langs.pkl'
+# try:
+#     with open(file_path, 'rb') as file:
+#         data = pickle.load(file)
+#     print(data) # This will print the loaded Python object
+# except Exception as e:
+#     print(f"Error loading pickle file: {e}")
